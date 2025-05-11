271a9beae46700dc45be4559a23a7d5f09b57e15
Umran Ozman <umonal@yahoo.com>
2025-05-11 10:47:48 -0400

    part- 1

---
+++ src/main/java/com/cydeo/TicketingProjectDataTekrar2025Application.java
@@ -0,0 +1,14 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingProjectDataTekrar2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketingProjectDataTekrar2025Application.class, args);
+	}
+
+
+}
