142085b1531c39a2d799284567b700c8b9e855bd
Umran Ozman <umonal@yahoo.com>
2025-05-13 20:40:58 -0400

    Runner Class mapper

--- src/main/java/com/cydeo/TicketingProjectDataTekrar2025Application.java
+++ src/main/java/com/cydeo/TicketingProjectDataTekrar2025Application.java
@@ -1,7 +1,9 @@
 package com.cydeo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TicketingProjectDataTekrar2025Application {
@@ -9,6 +11,9 @@ public class TicketingProjectDataTekrar2025Application {
 	public static void main(String[] args) {
 		SpringApplication.run(TicketingProjectDataTekrar2025Application.class, args);
 	}
-
+	@Bean
+	public ModelMapper mapper(){
+		return new ModelMapper();
+	}
 
 }
