9e9ecaffc1b4d87c5265358918d6afe3faf62cd9
Umran Ozman <umonal@yahoo.com>
2025-05-15 14:44:07 -0400

    RoleRepository interface updated

--- src/main/java/com/cydeo/repository/RoleRepository.java
+++ src/main/java/com/cydeo/repository/RoleRepository.java
@@ -1,12 +1,11 @@
 package com.cydeo.repository;
 
 import com.cydeo.entity.Role;
-import com.cydeo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
-public interface RoleRepository  extends JpaRepository<Role, Long>{
+@Repository
+public interface RoleRepository extends JpaRepository<Role,Long> {
 
      Role findByDescription(String description);
 
