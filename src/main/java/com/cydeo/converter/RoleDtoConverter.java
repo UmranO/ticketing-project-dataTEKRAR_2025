60b2800c20bc84b01a25c6c6970bc5e932a9fbd7
Umran Ozman <umonal@yahoo.com>
2025-05-15 14:11:32 -0400

    RoleDtoConverter class updated

--- src/main/java/com/cydeo/converter/RoleDtoConverter.java
+++ src/main/java/com/cydeo/converter/RoleDtoConverter.java
@@ -1,8 +1,8 @@
 package com.cydeo.converter;
-
 import com.cydeo.dto.RoleDTO;
 import com.cydeo.service.RoleService;
 import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
@@ -12,7 +12,7 @@ public class RoleDtoConverter implements Converter<String, RoleDTO> {
 
     RoleService roleService;
 
-    public RoleDtoConverter(RoleService roleService) {
+    public RoleDtoConverter(@Lazy RoleService roleService) {
         this.roleService = roleService;
     }
 
@@ -22,9 +22,6 @@ public class RoleDtoConverter implements Converter<String, RoleDTO> {
         if (source == null || source.equals("")) {  //  Select  -> ""
             return null;
         }
-
         return roleService.findById(Long.parseLong(source));
-
     }
-
 }
