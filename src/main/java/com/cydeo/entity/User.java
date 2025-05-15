69df233f134f4bacf828bddac68696b6689dd042
Umran Ozman <umonal@yahoo.com>
2025-05-15 14:42:38 -0400

    User class updated

--- src/main/java/com/cydeo/entity/User.java
+++ src/main/java/com/cydeo/entity/User.java
@@ -4,10 +4,8 @@ import com.cydeo.enums.Gender;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.Table;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Getter
@@ -23,27 +21,11 @@ public class User extends BaseEntity {
     private String passWord;
     private boolean enabled;
     private String phone;
-    @Enumerated(EnumType.STRING)
+
+    @ManyToOne
     private Role role;
+
     @Enumerated(EnumType.STRING)
     private Gender gender;
-
-    public User(Long id, LocalDateTime insertDateTime, Long insertUserId,
-                LocalDateTime lastUpdateDateTime,
-                Long lastUpdateUserId,
-                String firstName, String lastName,
-                String userName, String passWord,
-                boolean enabled, String phone,
-                Role role, Gender gender) {
-        super(id, insertDateTime, insertUserId, lastUpdateDateTime, lastUpdateUserId);
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.userName = userName;
-        this.passWord = passWord;
-        this.enabled = enabled;
-        this.phone = phone;
-        this.role = role;
-        this.gender = gender;
-    }
 
 }
