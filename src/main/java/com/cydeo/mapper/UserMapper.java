9be29f49306cc599d6492e38297ae6071a3ec7d3
Umran Ozman <umonal@yahoo.com>
2025-12-12 12:55:08 -0500

    UserMapper Class updated

--- src/main/java/com/cydeo/mapper/UserMapper.java
+++ src/main/java/com/cydeo/mapper/UserMapper.java
@@ -1,4 +1,12 @@
 package com.cydeo.mapper;
 
+import org.modelmapper.ModelMapper;
+
 public class UserMapper {
+
+    private final ModelMapper modelMapper;
+
+    public UserMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
 }
