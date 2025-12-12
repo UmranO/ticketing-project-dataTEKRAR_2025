72f439d056bb55ccb4cbb4fa35adc21438a4bfe8
Umran Ozman <umonal@yahoo.com>
2025-12-12 13:01:52 -0500

    UserMapper Class updated

--- src/main/java/com/cydeo/mapper/UserMapper.java
+++ src/main/java/com/cydeo/mapper/UserMapper.java
@@ -1,7 +1,10 @@
 package com.cydeo.mapper;
-
+import com.cydeo.dto.UserDTO;
+import com.cydeo.entity.User;
 import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
 
+@Component
 public class UserMapper {
 
     private final ModelMapper modelMapper;
@@ -9,4 +12,14 @@ public class UserMapper {
     public UserMapper(ModelMapper modelMapper) {
         this.modelMapper = modelMapper;
     }
+
+    public User convertToEntity(UserDTO dto){
+        return modelMapper.map(dto,User.class);
+
+    }
+
+    public UserDTO convertToDto(User entity){
+        return modelMapper.map(entity,UserDTO.class);
+    }
+
 }
