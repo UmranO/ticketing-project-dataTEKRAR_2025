49575d4d9b0b89d83933541da9c0f53bc82c37a1
Umran Ozman <umonal@yahoo.com>
2025-12-22 08:57:00 -0500

    BaseEntity class updated -isDeleted field added as false

--- src/main/java/com/cydeo/entity/BaseEntity.java
+++ src/main/java/com/cydeo/entity/BaseEntity.java
@@ -13,9 +13,13 @@ public class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private Boolean isDeleted = false;
+
     private LocalDateTime insertDateTime;
     private Long insertUserId;
     private LocalDateTime lastUpdateDateTime;
     private Long lastUpdateUserId;
+
 
 }
