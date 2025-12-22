1b61dcf6e017659134dbd1e439f8b2c17268d1a1
Umran Ozman <umonal@yahoo.com>
2025-12-22 16:56:50 -0500

    ProjectRepository Interface updated

--- src/main/java/com/cydeo/repository/ProjectRepository.java
+++ src/main/java/com/cydeo/repository/ProjectRepository.java
@@ -1,4 +1,9 @@
 package com.cydeo.repository;
 
-public class ProjectRepository {
+import com.cydeo.entity.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectRepository extends JpaRepository<Project,Long> {
+
+    Project findByProjectCode(String code);
 }
