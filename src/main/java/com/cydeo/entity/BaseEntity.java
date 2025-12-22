0aade4919eebe1a18c321824ed03e113e31f0c46
Umran Ozman <umonal@yahoo.com>
2025-12-22 14:30:21 -0500

    BaseEntity class updated

--- src/main/java/com/cydeo/entity/BaseEntity.java
+++ src/main/java/com/cydeo/entity/BaseEntity.java
@@ -16,19 +16,26 @@ public class BaseEntity {
 
     private Boolean isDeleted = false;
 
+    @Column(nullable = false,updatable = false)
     private LocalDateTime insertDateTime;
+    @Column(nullable = false,updatable = false)
     private Long insertUserId;
+    @Column(nullable = false)
     private LocalDateTime lastUpdateDateTime;
+    @Column(nullable = false)
     private Long lastUpdateUserId;
 
 
+    @PrePersist
     private void onPrePersist(){
         this.insertDateTime=LocalDateTime.now();
         this.lastUpdateDateTime=LocalDateTime.now();
         this.insertUserId=1L;
         this.lastUpdateUserId=1L;
     }
-
+    //Asagidaki onPrePersist()'den sonra calistigi icin & bu metod'da  this.insertDateTime=LocalDateTime.now(); VE
+    // this.insertUserId=1L; field'leri olmadigi icin null getiriyor.
+    @PreUpdate
     private void onPreUpdate(){
         this.lastUpdateDateTime=LocalDateTime.now();
         this.lastUpdateUserId=1L;
