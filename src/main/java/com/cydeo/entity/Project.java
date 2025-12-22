51e08eff83a6a42fcffe63bd62d2fad6899f4533
Umran Ozman <umonal@yahoo.com>
2025-12-22 15:03:51 -0500

    Project Entity Class updated

--- src/main/java/com/cydeo/entity/Project.java
+++ src/main/java/com/cydeo/entity/Project.java
@@ -1,13 +1,14 @@
 package com.cydeo.entity;
 
+import com.cydeo.enums.Status;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.Where;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.time.LocalDate;
 
 @Getter
 @Setter
@@ -17,10 +18,19 @@ import javax.persistence.Table;
 @Where(clause = "is_deleted=false")
 public class Project extends BaseEntity {
 
+
     private String projectCode;
     private String projectName;
 
+    @Column(columnDefinition = "DATE")
+    private LocalDate startDate;
 
+    @Column(columnDefinition = "DATE")
+    private LocalDate endDate;
 
+    @Enumerated(EnumType.STRING)
+    private Status projectStatus;
+
+    private String projectDetail;
 
 }
