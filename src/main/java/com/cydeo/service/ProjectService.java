8281e20e0e86b1d06f2444ed487a73d305609517
Umran Ozman <umonal@yahoo.com>
2025-12-23 14:08:36 -0500

    ProjectServices updated

--- src/main/java/com/cydeo/service/ProjectService.java
+++ src/main/java/com/cydeo/service/ProjectService.java
@@ -4,11 +4,12 @@ import java.util.List;
 
 public interface ProjectService {
 
-    ProjectDTO getByProjectCode(String code);
-    List<ProjectDTO> listAllProjects();
+    ProjectDTO getByProjectCode(String code);            //Since Service it reeturns DTO
+    List<ProjectDTO> listAllProjects();                  //Since we have Table in the Project Create /UI
     void save(ProjectDTO dto);
     void update(ProjectDTO dto);
     void delete(String code);
+
     void complete(String projectCode);
 
 
