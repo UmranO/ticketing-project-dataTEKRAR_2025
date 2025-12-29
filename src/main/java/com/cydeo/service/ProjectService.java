cc06d9e75eb86cb29ce6da85b40087d54a5a42ec
Umran Ozman <umonal@yahoo.com>
2025-12-29 14:11:54 -0500

    ProjectService Class updated

--- src/main/java/com/cydeo/service/ProjectService.java
+++ src/main/java/com/cydeo/service/ProjectService.java
@@ -4,13 +4,13 @@ import java.util.List;
 
 public interface ProjectService {
 
-    ProjectDTO getByProjectCode(String code);            //Since Service it reeturns DTO
+    ProjectDTO getByProjectCode(String code);            //Since Service it returns DTO
     List<ProjectDTO> listAllProjects();                  //Since we have Table in the Project Create /UI
     void save(ProjectDTO dto);
     void update(ProjectDTO dto);
     void delete(String code);
 
-    void complete(String projectCode);
+    void complete(String projectCode);                   //Later we figured out we need complete() so came here & write
 
 
 }
