aaf9ce18a35343c2d236914cd0c2a77822ccc51d
Umran Ozman <umonal@yahoo.com>
2025-12-30 17:14:11 -0500

    TaskServiceImpl Class

--- src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
@@ -1,4 +1,76 @@
 package com.cydeo.service.impl;
+import com.cydeo.dto.TaskDTO;
+import com.cydeo.entity.Task;
+import com.cydeo.enums.Status;
+import com.cydeo.mapper.TaskMapper;
+import com.cydeo.repository.TaskRepository;
+import com.cydeo.service.TaskService;
+import org.springframework.stereotype.Service;
 
-public class TaskServiceImpl {
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class TaskServiceImpl implements TaskService {
+
+    private final TaskRepository taskRepository;
+    private final TaskMapper taskMapper;
+
+    public TaskServiceImpl(TaskRepository taskRepository, TaskMapper taskMapper) {  //By injection we're doing memory-level object reference assignment.
+        this.taskRepository = taskRepository;                                       //It holds a reference to an existing object
+        this.taskMapper = taskMapper;                                               //So we can call methods on it immediately
+    }
+
+    @Override
+    public List<TaskDTO> listAllTasks() {       //we're getting all the tasks from TaklkRepo/stream/mapping each to a DTO/Collect to a List
+        return taskRepository.findAll().stream().map(taskMapper::convertToDto).collect(Collectors.toList());
+    }
+
+    @Override
+    public void save(TaskDTO dto) {
+
+        dto.setTaskStatus(Status.OPEN);              //Since we don't have a place to give the status in the Form we're setting the status
+        dto.setAssignedDate(LocalDate.now());        //Since we don't have a place to give the status in the Form we're setting AssignedDate
+        Task task = taskMapper.convertToEntity(dto); //mapping Dto to Entity
+        taskRepository.save(task);                   //saving to DB
+    }
+
+    @Override
+    public void update(TaskDTO dto) {
+
+        Optional<Task> task = taskRepository.findById(dto.getId());      //Repo's findById(Long Id) returns Optional
+        Task convertedTask  = taskMapper.convertToEntity(dto);           //We convert the editted dto to entity
+
+        if(task.isPresent()){                                            //Since optional
+            convertedTask.setTaskStatus(task.get().getTaskStatus());     //Setting the status which is missing in the Form
+            convertedTask.setAssignedDate(task.get().getAssignedDate()); //Setting the AssignedDate which is missing in the Form
+            taskRepository.save(convertedTask);                          //saving to DB
+        }
+    }
+
+    @Override
+    public void delete(Long id) {
+
+        Optional<Task> foundTask = taskRepository.findById(id);          //Repo's findById(Long Id) returns Optional
+
+        if(foundTask.isPresent()){                                       //Since optional
+            foundTask.get().setIsDeleted(true);                          //Doing a soft delete
+            taskRepository.save(foundTask.get());                        //with get() at the end we Optional->Task
+        }
+    }
+
+    @Override
+    public TaskDTO findById(Long id) {
+
+        Optional<Task> task = taskRepository.findById(id);
+
+        if(task.isPresent()){
+            return taskMapper.convertToDto(task.get());
+        }
+        return null;                                                    //If not present return null
+    }
+
+
 }
