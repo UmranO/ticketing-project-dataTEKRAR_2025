bf830ad678ab00437c9b3484e07b9a956bb04ad5
Umran Ozman <umonal@yahoo.com>
2025-12-30 13:31:09 -0500

    TaskService Interface is updated

--- src/main/java/com/cydeo/service/TaskService.java
+++ src/main/java/com/cydeo/service/TaskService.java
@@ -1,4 +1,25 @@
 package com.cydeo.service;
 
-public interface TaskService {
+import com.cydeo.dto.TaskDTO;
+
+import java.util.List;
+
+public interface TaskService {                                       //Burasi service oldugu icin DTO ile calisiyor.
+                                                                     //So verdigimiz/aldigimiz hersey DTO olmali!!
+
+    //save
+    void save(TaskDTO dto);
+
+    //ListAll
+    List<TaskDTO> listAllTasks();
+
+    //Update
+    void update(TaskDTO dto);
+
+    //Delete
+    void delete(Long id);
+
+    //findById
+    TaskDTO findById(Long id);
+
 }
