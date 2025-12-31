4cc7266395ee9cbea62d7229f1e70e1734a4fc1a
Umran Ozman <umonal@yahoo.com>
2025-12-30 20:48:07 -0500

    ProjectRepository updated

--- src/main/java/com/cydeo/repository/ProjectRepository.java
+++ src/main/java/com/cydeo/repository/ProjectRepository.java
@@ -1,9 +1,15 @@
 package com.cydeo.repository;
 import com.cydeo.entity.Project;
+import com.cydeo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface ProjectRepository extends JpaRepository<Project,Long> {  //Here we give the Entity that will be working
                                                                           //with this Repo.& the Primary is Long(DB icin)
     Project findByProjectCode(String code);                               //Here the Project Code is Unique & it returns
                                                                           //Entity(Buradaki uniqe UI icin)
+
+
+    List<Project> findAllByAssignedManager(User manager);
 }
