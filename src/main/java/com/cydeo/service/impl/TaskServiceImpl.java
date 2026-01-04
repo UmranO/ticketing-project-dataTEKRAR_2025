de97a508c5c66edac911c6a9f88fe8a8ae9f073b
Umran Ozman <umonal@yahoo.com>
2026-01-04 17:13:55 -0500

    TaskServiceImpl Class updated

--- src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
@@ -1,7 +1,10 @@
 package com.cydeo.service.impl;
+import com.cydeo.dto.ProjectDTO;
 import com.cydeo.dto.TaskDTO;
+import com.cydeo.entity.Project;
 import com.cydeo.entity.Task;
 import com.cydeo.enums.Status;
+import com.cydeo.mapper.ProjectMapper;
 import com.cydeo.mapper.TaskMapper;
 import com.cydeo.repository.TaskRepository;
 import com.cydeo.service.TaskService;
@@ -17,10 +20,12 @@ public class TaskServiceImpl implements TaskService {
 
     private final TaskRepository taskRepository;
     private final TaskMapper taskMapper;
+    private final ProjectMapper projectMapper;
 
-    public TaskServiceImpl(TaskRepository taskRepository, TaskMapper taskMapper) {  //By injection we're doing memory-level object reference assignment.
+    public TaskServiceImpl(TaskRepository taskRepository, TaskMapper taskMapper, ProjectMapper projectMapper) {  //By injection we're doing memory-level object reference assignment.
         this.taskRepository = taskRepository;                                       //It holds a reference to an existing object
         this.taskMapper = taskMapper;                                               //So we can call methods on it immediately
+        this.projectMapper = projectMapper;
     }
 
     @Override
@@ -82,6 +87,18 @@ public class TaskServiceImpl implements TaskService {
     @Override
     public int totalCompletedTask(String projectCode) {
         return taskRepository.totalCompletedTasks(projectCode);
+    }
+
+    @Override
+    public void deleteByProject(ProjectDTO projectDTO) {
+        Project project = projectMapper.convertToEntity(projectDTO);
+        List<Task> tasks = taskRepository.findAllByProject(project);
+        tasks.forEach(task -> delete(task.getId()));
+        }
+
+    @Override
+    public void completeByProject(ProjectDTO projectDTO) {
+
     }
 
 
