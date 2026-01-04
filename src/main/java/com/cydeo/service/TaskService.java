f6b6a57440f1ab4767879b776bd8f7c6eee254c5
Umran Ozman <umonal@yahoo.com>
2026-01-04 17:12:33 -0500

    TaskService Interface updated

--- src/main/java/com/cydeo/service/TaskService.java
+++ src/main/java/com/cydeo/service/TaskService.java
@@ -28,4 +28,6 @@ public interface TaskService {                                       //Burasi se
 
     void deleteByProject(ProjectDTO projectDTO);
 
+    void completeByProject(ProjectDTO projectDTO);
+
 }
