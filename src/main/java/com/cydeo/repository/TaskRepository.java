c9fe70b113eceda9dada7782adbbf8ab9fc893f6
Umran Ozman <umonal@yahoo.com>
2026-01-05 15:38:33 -0500

    TaskRepository Interface updated

--- src/main/java/com/cydeo/repository/TaskRepository.java
+++ src/main/java/com/cydeo/repository/TaskRepository.java
@@ -2,6 +2,8 @@ package com.cydeo.repository;
 
 import com.cydeo.entity.Project;
 import com.cydeo.entity.Task;
+import com.cydeo.entity.User;
+import com.cydeo.enums.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -13,12 +15,24 @@ public interface TaskRepository extends JpaRepository<Task,Long> {
 
     @Query("SELECT COUNT(t) FROM Task t WHERE t.project.projectCode = ?1 AND t.taskStatus <> 'COMPLETE'")
     int totalNonCompletedTasks(String projectCode);
+//----------------------------------------------------------------------------------------------------------------------
 
     @Query(value = "SELECT COUNT(*) " +
             "FROM tasks t JOIN projects p on t.project_id=p.id " +
             "WHERE p.project_code=?1 AND t.task_status='COMPLETE'",nativeQuery = true)
+
+//----------------------------------------------------------------------------------------------------------------------
     int totalCompletedTasks(String projectCode);
-
+//----------------------------------------------------------------------------------------------------------------------
     List<Task> findAllByProject(Project project);
+//----------------------------------------------------------------------------------------------------------------------
+    List<Task> findAllByTaskStatusIsNotAndAssignedEmployee(Status status, User user);
+//------------------------------------------------------------------------------------
 
+//Asagisi benim Pending Task icin yaptigim. CT farkli yapmis. Bunu comment edecegim yukardaki CT'nin ki. O hem status
+//hem de employee'yi secen bir () create etmis. Ben status'u stream ile filter ederek yaptim TaskServiceImpl'de. fark bu
+
+    //List<Task> findAllByAssignedEmployee(User employee);
+
+//----------------------------------------------------------------------------------------------------------------------
 }
