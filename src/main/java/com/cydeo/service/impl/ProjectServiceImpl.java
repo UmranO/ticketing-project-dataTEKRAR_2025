40c3ff6579f025521eb1dc443a2da5da4a39f256
Umran Ozman <umonal@yahoo.com>
2026-01-07 16:14:29 -0500

    ProjectServiceImpl Class updated

--- src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
@@ -166,6 +166,14 @@ public class ProjectServiceImpl implements ProjectService {
 
              ).collect(Collectors.toList());
          }
+
+    @Override
+    public List<ProjectDTO> listAllNonCompletedByAssignedManager(UserDTO assignedManager) {
+        List<Project> projects = projectRepository
+                .findAllByProjectStatusIsNotAndAssignedManager(Status.COMPLETE, userMapper.convertToEntity(assignedManager));
+        return projects.stream().map(projectMapper::convertToDto).collect(Collectors.toList());
+    }
+
 //--Yukardakinin commentsiz hali----------------------------------------------------------------------------------------
 //    @Override
 //    public List<ProjectDTO> listAllProjectDetails() {
