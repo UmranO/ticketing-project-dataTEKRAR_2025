47e02e7e024f3c2480059fcaba794a515b8c9223
Umran Ozman <umonal@yahoo.com>
2026-01-09 13:14:50 -0500

    UserService Interface updated

--- src/main/java/com/cydeo/service/UserService.java
+++ src/main/java/com/cydeo/service/UserService.java
@@ -10,7 +10,7 @@ public interface UserService  {
     List<UserDTO> listAllUsers();
     UserDTO findByUserName(String username);
     void save(UserDTO user);
-    void deleteByUserName(String username);     //this deletes from both the DB & the UI - sonradan asagidakine degistirdik
+ //   void deleteByUserName(String username);     //this deletes from both the DB & the UI - sonradan asagidakine degistirdik
     UserDTO update(UserDTO user);
     void delete(String username);               //we added this to show how to keep in DB but delete from UI-SOFT Delete
 
