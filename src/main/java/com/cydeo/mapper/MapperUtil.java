996cf9677e31248be3e039585ed15c95b5f0af2c
Umran Ozman <umonal@yahoo.com>
2026-01-10 16:08:32 -0500

    MapperUtil Class updated

--- src/main/java/com/cydeo/mapper/MapperUtil.java
+++ src/main/java/com/cydeo/mapper/MapperUtil.java
@@ -3,13 +3,28 @@ package com.cydeo.mapper;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
+import java.lang.reflect.Type;
+
 @Component
 public class MapperUtil {
     ModelMapper modelMapper;
 
     public MapperUtil(ModelMapper modelMapper){
         this.modelMapper=modelMapper;
-
     }
+
+    // Some comments to explain what it does. CT prefers this alternative:
+    public <T> T convert(Object objectToBeConverted, T convertedObject) {
+        return modelMapper.map(objectToBeConverted, (Type) convertedObject.getClass());
+    }
+
+//    public <T> T convert(Object objectToBeConverted, Class<T> convertedObject) { //UO:There's a Class class & this convertedObject
+//        return modelMapper.map(objectToBeConverted, convertedObject);            //is a variable holding the Class object eg:TaskDTO.class
+                                                                                   //& by writing Class<T> in front of it we are saying that
+                                                                                   //his variable is a Class object representing type T & in
+                                                                                   //reality it'll be whatever we pass for T
+//    }
+
+
 
 }
